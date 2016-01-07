30d3eea5c444c08d58c163b6fa9442fc659aa6cc
Mad Hatter <hatter@factionlabs.io>
2016-01-07 00:53:43 -0500

    minor ui update: added logo
    Signed-off-by: Mad Hatter <hatter@factionlabs.io>

--- public/src/app/welcome.js
+++ public/src/app/welcome.js
@@ -3,7 +3,13 @@ var React = require('react');
 module.exports = React.createClass({
     render: function () {
         return (
-            <div className="ui grid">
+            <div className="ui">
+                <div className="center">
+                    <h1 className="header">Meld Game Management</h1>
+                </div>
+                <div className="center">
+                    <img src="./assets/images/meld_256.png"/>
+                </div>
             </div>
         )
     }
